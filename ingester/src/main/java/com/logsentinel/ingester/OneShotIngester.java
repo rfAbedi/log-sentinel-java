830af5708acc60bfb90bd7ee9496c5e2dc0e356d
Erfan <rfabedii@gmail.com>
2026-07-24 21:40:21 +0330

    feat(ingester): delete files after successful publishing

--- ingester/src/main/java/com/logsentinel/ingester/OneShotIngester.java
+++ ingester/src/main/java/com/logsentinel/ingester/OneShotIngester.java
@@ -5,7 +5,10 @@ import com.logsentinel.ingester.file.LogFileContent;
 import com.logsentinel.ingester.processing.LogFileProcessor;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public final class OneShotIngester {
 
@@ -19,9 +22,28 @@ public final class OneShotIngester {
         this.fileProcessor = fileProcessor;
     }
 
-    public int run() throws IOException {
+    public int run() throws IOException, InterruptedException, ExecutionException {
         List<LogFileContent> files = fileReader.readExisting();
-        files.forEach(fileProcessor::process);
-        return files.size();
+        int processedFiles = 0;
+
+        for (LogFileContent file : files) {
+            waitForAll(fileProcessor.process(file));
+            Files.delete(file.path());
+            processedFiles++;
+        }
+
+        return processedFiles;
+    }
+
+    private void waitForAll(List<Future<?>> publishResults)
+            throws InterruptedException, ExecutionException {
+        try {
+            for (Future<?> publishResult : publishResults) {
+                publishResult.get();
+            }
+        } catch (InterruptedException exception) {
+            Thread.currentThread().interrupt();
+            throw exception;
+        }
     }
 }
